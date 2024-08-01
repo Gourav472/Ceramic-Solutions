d06278a163f9beddba0871fa367094015df601bd
vikas-jakhar <vikashjakhar2004@gmail.com>
2024-08-01 16:50:01 +0530

    blog section update

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
-import logo from './logo.svg';
 import './App.css';
+import Blog from './components/Blog';
 import Header from './components/Header';
 
 function App() {
   return (
     <>
+      <Blog />
     </>
   );
 }
---
+++ src/components/Blog.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import CommonHeading from '../common/CommonHeading'
+
+const Blog = () => {
+    return (
+        <div className='py-1'>
+            <div className="container">
+                <CommonHeading HeadingClass="" HeadingName="Our Blog" />
+            </div>
+        </div>
+    )
+}
+
+export default Blog
