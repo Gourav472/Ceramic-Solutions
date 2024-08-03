7c96ed479641c1b37966da40cbab2875817dfefb
Gourav472 <149783900+Gourav472@users.noreply.github.com>
2024-08-03 10:05:55 +0530

    component name changed

--- src/App.js
+++ src/App.js
@@ -1,22 +1,14 @@
 import './App.css';
-<<<<<<< HEAD
 import Blog from './components/Blog';
 import Header from './components/Header';
-import Chooseus from './components/Chooseus';
-=======
 import Choose from './components/Choose';
->>>>>>> 0ecd53b60f0a0fb9c22cb13bb3b193363cd1654f
 
 function App() {
   return (
     <>
-<<<<<<< HEAD
       <Header />
       <Blog />
-    <Chooseus/>
-=======
-    <Choose/>
->>>>>>> 0ecd53b60f0a0fb9c22cb13bb3b193363cd1654f
+      <Choose />
     </>
   );
 }
