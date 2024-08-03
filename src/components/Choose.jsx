6692d8921d9bc88bb426681b91e06d42ee5a8df2
akshaybishnoi01 <akshaybishnoi0008@gmail.com>
2024-08-03 10:43:23 +0530

    change icon

--- src/components/Choose.jsx
+++ src/components/Choose.jsx
@@ -1,5 +1,6 @@
 import React from 'react'
-import CommonHeading from '../common/CommonHeading'
+import CommonHeading from '../common/CommonHeading';
+import Icon  from "../common/Icons";
 
 const Choose = () => {
   return (
@@ -11,11 +12,33 @@ const Choose = () => {
                 <p className='max-w-[590px] text-black font-normal text-lg leading-7 font-jakarta pt-5'>
                 Lörem ipsum koda astrobel: sutaveligen. Rodod bänera viliga. Pregigt primasofi dede facebooka: förutom tivaligt. 
                 </p>
-                <div className='flex gap-5'>
-                  
+                <div className='flex gap-5 items-start pt-8'>
+                  <Icon iconName="StarIcon" className="cursor-pointer flex" />
                   <div>
-                    <p className='max-w-[210px] text-black font-semibold text-xl leading-8 font-jakarta'>
+                    <p className='text-black font-semibold text-xl leading-8 font-jakarta'>
                     High Quality Materials
+                    </p>
+                    <p className='max-w-[370px] font-jakarta font-normal text-base leading-6 text-black pt-3'>
+                    Es ist ein lang erwiesener Fakt, dass ein Leser vom Text abgelenkt wird.
+                    </p>
+                  </div>
+                </div>
+                <div className='flex gap-5 items-start pt-8'>
+                  <Icon iconName="LikeIcon" className="cursor-pointer flex" />
+                  <div>
+                    <p className='text-black font-semibold text-xl leading-8 font-jakarta'>
+                    Durability with Warranty
+                    </p>
+                    <p className='max-w-[370px] font-jakarta font-normal text-base leading-6 text-black pt-3'>
+                    Es ist ein lang erwiesener Fakt, dass ein Leser vom Text abgelenkt wird.
+                    </p>
+                  </div>
+                </div>
+                <div className='flex gap-5 items-start pt-8'>
+                  <Icon iconName="PhoneIcon" className="cursor-pointer flex" />
+                  <div>
+                    <p className='text-black font-semibold text-xl leading-8 font-jakarta'>
+                    24/7 Support
                     </p>
                     <p className='max-w-[370px] font-jakarta font-normal text-base leading-6 text-black pt-3'>
                     Es ist ein lang erwiesener Fakt, dass ein Leser vom Text abgelenkt wird.
